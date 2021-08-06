1edb39a8a3e76d2a3c53f28a0d0bd7a540289c86
杨浩 <871626535@qq.com>
2021-08-06 16:22:54 +0800

    更新

---
+++ app/src/main/java/com/example/yanghao/tourcommercialtenant/BaseActivity.kt
@@ -0,0 +1,21 @@
+package com.example.yanghao.tourcommercialtenant
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ *    @author : younghow
+ *    @date   : 2021/8/6 16:20
+ *    description:
+ */
+class BaseActivity : AppCompatActivity() {
+
+    
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+
+}
